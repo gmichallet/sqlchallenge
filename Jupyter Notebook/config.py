9ea1ae2c3e12d45d1e78047a98b680de359c20b1
gmichallet <75396648+gmichallet@users.noreply.github.com>
2021-02-24 13:18:01 -0500

    Add files via upload

---
+++ Jupyter Notebook/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# default settings
+
+ENV = "prod" # optional field with a default value
+
+DB_NAME = "db"
+DB_HOST = "127.0.0.1"
+DB_USER = "postgres"
+DB_PASS = None # even None could be given as default value
+
+SECRET_KEY:str # Python 3 only
